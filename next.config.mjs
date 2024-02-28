b8cb9e5535714fd813d3152baa044a06e73aaeb5
Mohamad-Mahdi Tasha <imwhdiiii@gmail.com>
2024-02-28 16:57:40 +0330

    added comments in some fils

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
+// Codes by mahdi tasha
+// Importing configs of nextJS
 /** @type {import('next').NextConfig} */
+
+// Defining configs
 const nextConfig = {};
 
+// Exporting the configs
 export default nextConfig;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,13 @@
+// Codes by mahdi tasha
+// Importing configs of tailwindCSS
 import type { Config } from "tailwindcss";
 
+// Defining configs
 const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
+  content: [],
+  theme: {},
   plugins: [],
 };
+
+// Exporting the configs
 export default config;
