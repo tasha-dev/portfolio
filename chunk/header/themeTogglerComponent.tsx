8115f9927862426958b9854700494590448a89da
Mohamad-Mahdi Tasha <imwhdiiii@gmail.com>
2024-02-29 06:09:48 +0330

    completed theme toggler button in header

--- chunk/header/themeTogglerComponent.tsx
+++ chunk/header/themeTogglerComponent.tsx
@@ -3,24 +3,31 @@
 'use client';
 
 // Importing part
-import {ReactNode} from "react";
+import {ReactNode, useState} from "react";
+import IconComponent from "@/chunk/iconComponent";
 
 // Creating and exporting theme Toggler as default
 export default function ThemeTogglerComponent():ReactNode {
+    // Defining state of component
+    const [theme, setTheme] = useState<'light' | 'dark'>('dark');
+
     // Returning JSX
     return (
         <button
             onClick={() => {
                 const htmlElement = document.querySelector('html');
-                const theme = (htmlElement?.classList.contains('dark')) ? 'dark' : 'light';
 
-                (theme === 'dark')
-                    ? htmlElement?.classList.remove('dark')
-                    : htmlElement?.classList.add('dark');
+                if (theme === 'dark') {
+                    htmlElement?.classList.remove('dark');
+                    setTheme('light');
+                } else {
+                    htmlElement?.classList.add('dark');
+                    setTheme('dark');
+                }
             }}
-            className={'w-[50px] h-[50px] flex items-center justify-center dark:bg-light bg-dark border dark:border-light/20 border-dark:20 rounded-[10px]'}
+            className={'w-[50px] h-[50px] flex items-center justify-center dark:bg-light bg-dark border dark:border-light/20 border-dark:20 rounded-[10px] dark:text-dark text-light'}
         >
-
+            <IconComponent name={(theme === 'dark') ? 'sun' : 'moon'} />
         </button>
     );
 }
