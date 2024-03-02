fa9b5cb31629bfc4d22bef84526896f7d8ba5108
Mohamad-Mahdi Tasha <imwhdiiii@gmail.com>
2024-03-02 16:57:33 +0330

    created root layout of the pages

---
+++ app/layout.tsx
@@ -0,0 +1,51 @@
+// Codes by mahdi tasha
+// Importing part
+import {ReactNode} from "react";
+import {cn} from '@/lib/cn';
+import {Montserrat} from "next/font/google";
+import siteConfig from "@/config/siteConfig";
+
+// Defining fonts
+const montserratFont = Montserrat({
+    weight: ['700', '500', '300'],
+    variable: "--font-sans"
+})
+
+// Defining metadata
+export const metadata = {
+    title: {
+        default: 'Home',
+        template: `%s | Mahdi Tasha`,
+    },
+    description: siteConfig.description,
+    keywords: ["Mahdi Tasha", "Front End Developer", "Web Development", "HTML", "CSS", "JavaScript", "React", "Responsive Design", "User Interface", "UX Design", "Web Portfolio", "Code Quality", "Innovation", "Creative Development", "Midlevel Developer", "Web Experiences", "Clean Code", "Interactive Design", "Technical Expertise"],
+    themeColor: [
+        {media: "(prefers-color-scheme: light)", color: "#fffff"},
+        {media: "(prefers-color-scheme: dark)", color: "#00000"},
+    ],
+    openGraph: {
+        type: "website",
+        locale: "en_US",
+        url: siteConfig.url,
+        title: siteConfig.name,
+        description: siteConfig.description,
+        siteName: siteConfig.name,
+    }
+}
+
+// Defining type of props
+interface propsType {
+    children: ReactNode;
+}
+
+// Creating and exporting the root layout component
+export default function RootLayout({children}:propsType):ReactNode {
+    // Returning JSX
+    return (
+        <html>
+            <body className={cn('overflow-x-hidden min-h-screen font-sans', montserratFont.variable)}>
+                {children}
+            </body>
+        </html>
+    );
+}
