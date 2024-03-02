d4b28587dfcc778bf4a9dc1c67e0eb1225a550d0
Mohamad-Mahdi Tasha <imwhdiiii@gmail.com>
2024-03-02 17:01:30 +0330

    changed some configs in tailwind config file

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,10 +6,10 @@ import type { Config } from "tailwindcss"
 const config = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
+    './component/**/*.{ts,tsx}',
+    './component/*.{ts,tsx}',
+    './app/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}'
   ],
   prefix: "",
   theme: {
