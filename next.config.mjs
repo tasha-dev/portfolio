6e9b963919e0afe181e6cb039b7f6ac3a43a9926
Mohamad-Mahdi Tasha <imwhdiiii@gmail.com>
2024-03-02 16:22:24 +0330

    removed all files to start over via shadcn

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-// Codes by mahdi tasha
-// Importing configs of nextJS
-/** @type {import('next').NextConfig} */
-
-// Defining configs
-const nextConfig = {};
-
-// Exporting the configs
-export default nextConfig;
