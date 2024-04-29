a6c00524e26dfbae695374e19643a99a89da4827
Mohamad-Mahdi Tasha <imwhdiiii@gmail.com>
2024-04-29 12:28:00 +0330

    added 3d cube effect with spline

--- component/page.tsx
+++ component/page.tsx
@@ -8,6 +8,7 @@ import {motion} from 'framer-motion';
 import Container from "@/component/ui/container";
 import {pageType} from "@/types/uiComponent";
 import Footer from "@/component/footer";
+import Spline from '@splinetool/react-spline';
 
 // Creating and exporting page component as default
 export default function Page({children, className}:pageType):ReactNode {
@@ -21,8 +22,13 @@ export default function Page({children, className}:pageType):ReactNode {
             initial={'hidden'}
             animate={'showing'}
             className={className}
-        >
-            <Container size={'sm'} className={'lg:mt-[100px] mt-0'}>
+        > 
+            <Container size={'sm'} className={'lg:mt-[100px] mt-0 relative'}>
+                <Spline 
+                    className={"pointer-events-none absolute right-full lg:-translate-x-full -translate-x-[50%] lg:-translate-y-[50%] -translate-y-[20%] top-0 -z-20"} 
+                    scene="https://prod.spline.design/jVm5Adpd7dFtuCQR/scene.splinecode" 
+                    style={{width: '75%', height: '30%'}}
+                />
                 {children}
                 <Footer />
             </Container>
