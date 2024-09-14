58df5bd837d682ea27890e7ce05b49fbb173b3cc
Mohamad-Mahdi Tasha <imwhdiiii@gmail.com>
2024-09-14 15:39:53 +0330

    removed all files to start over

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-// Codes by mahdi taha
-// Importing type of configs of next.js
-/** @type {import('next').NextConfig} */
-
-// Defining the configs
-const nextConfig = {};
-
-// Exporting the configs as default
-export default nextConfig;
--- util/cn.ts
+++
@@ -1,9 +0,0 @@
-// Codes by mahdi tasha
-// Importing part
-import { type ClassValue, clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
-
-// Creating and exporting cn function to combine tailwindcss classnames
-export function cn(...inputs: ClassValue[]) {
-    return twMerge(clsx(inputs))
-}
