9d268fd2f04e0dd171935951d0bfd9f91a412933
Mohamad-Mahdi Tasha <imwhdiiii@gmail.com>
2024-09-15 15:55:23 +0330

    completed header of the pages

--- component/header.tsx
+++ component/header.tsx
@@ -4,6 +4,7 @@ import {ReactNode} from "react";
 import Logo from "@/component/ui/logo";
 import Container from "@/component/ui/container";
 import ThemeToggle from "@/component/ui/themeToggle";
+import Link from "next/link";
 
 // Creating and exporting header component as default
 export default function Header():ReactNode {
@@ -12,7 +13,12 @@ export default function Header():ReactNode {
         <header className={'backdrop-blur-2xl dark:bg-white/20 bg-black/20'}>
             <Container className={'flex items-center justify-between'}>
                 <Logo />
-                <ThemeToggle />
+                <div className={'flex items-center justify-center gap-5'}>
+                    <Link className={'link-text'} href={'/'}>Home</Link>
+                    <Link className={'link-text'} href={'/blog'}>Blog</Link>
+                    <Link className={'link-text'} href={'/contact'}>Contact</Link>
+                    <ThemeToggle />
+                </div>
             </Container>
         </header>
     );
