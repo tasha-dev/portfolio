9809f57ef26ed2133f2dd5e179e0db1ed09a6530
Mohamad-Mahdi Tasha <imwhdiiii@gmail.com>
2025-09-07 12:35:30 +0330

    added some config files

---
+++ type/component.ts
@@ -0,0 +1,8 @@
+// Codes by mahdi tasha
+// Importing part
+import { ReactNode } from "react";
+
+// Creating and exporting Type of props of components
+export interface RootLayoutProps {
+  children: ReactNode;
+}
