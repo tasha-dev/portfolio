609a3d497f9e35a6a1a65eef07a0a6963fe87ce4
Mohamad-Mahdi Tasha <imwhdiiii@gmail.com>
2026-07-14 11:54:11 +0330

    removed everything to start over

--- next.config.ts
+++
@@ -1,9 +0,0 @@
-// Codes by mahdi tasha
-// Importing part
-import type { NextConfig } from "next";
-
-// Defininedg the configs of next.js
-const nextConfig: NextConfig = {};
-
-// Exporting the configs as default
-export default nextConfig;
--- postcss.config.mjs
+++
@@ -1,8 +0,0 @@
-// Codes by mahdi tasha
-// Defining configs of postcss
-const config = {
-  plugins: ["@tailwindcss/postcss"],
-};
-
-// Exporting the configs as default
-export default config;
