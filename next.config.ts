037b9c20356524d4944a038cafc78dfbec6d8338
Mohamad-Mahdi Tasha <imwhdiiii@gmail.com>
2026-07-14 13:15:26 +0330

    Added comments for config files

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
+// Codes by mahdi tasha
+// Importing part
 import type { NextConfig } from "next";
 
+// Defining configs of next.js
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+   reactCompiler: true,
 };
 
+// Exporting the configs as default
 export default nextConfig;
--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,10 @@
+// Codes by mahdi tasha
+// Creating postcss config
 const config = {
-  plugins: {
-    "@tailwindcss/postcss": {},
-  },
+   plugins: {
+      "@tailwindcss/postcss": {},
+   },
 };
 
+// Exporting the config as default
 export default config;
