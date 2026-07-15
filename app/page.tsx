6a705e4aa52999f9c9948f83470e3a3f18704725
Mohamad-Mahdi Tasha <imwhdiiii@gmail.com>
2026-07-15 12:08:57 +0330

    Added footer and seprated each section in mobile view

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 // Importing part
 import Header from "@/component/header";
 import { Contact } from "@/component/section/contact";
+import Footer from "@/component/section/footer";
 import Hero from "@/component/section/hero";
 import { Projects } from "@/component/section/projects";
 import Skills from "@/component/section/skils";
@@ -13,9 +14,10 @@ export default function HomePage() {
       <>
          <Header />
          <Hero />
-         <Skills className="lg:mb-8 mb-5" />
-         <Projects />
-         <Contact />
+         <Skills className="lg:mb-8 mb-5 lg:mt-0 mt-5" />
+         <Projects className="lg:border-0 border-t border-t-foreground/20" />
+         <Contact className="lg:border-0 border-t border-t-foreground/20" />
+         <Footer />
       </>
    );
 }
---
+++ component/section/footer.tsx
@@ -0,0 +1,23 @@
+// Codes by mahdi tasha
+// Importing part
+import { ClassOnlyProps } from "@/type/component";
+import moment from "moment";
+import { cn } from "@/lib/util";
+
+// Creating and exporting Footer section component as default
+export default function Footer({ className }: ClassOnlyProps) {
+   // Returning JSX
+   return (
+      <footer className={cn("border-t p-4 border-t-foreground/20", className)}>
+         <main className="max-w-2xl w-full prose prose-neutral dark:prose-invert mx-auto">
+            <h4 className="lg:text-center">
+               copyright: © {moment().format("YYYY")} Mahdi Tasha. All rights
+               reserved.
+            </h4>
+            <h6 className="mt-2 lg:text-center">
+               Built with Next.js, TypeScript & Tailwind CSS — hosted on Vercel.
+            </h6>
+         </main>
+      </footer>
+   );
+}
