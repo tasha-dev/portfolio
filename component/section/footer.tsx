b20d87615243ba67ca78d143f86fce0048a1ce3e
Mohamad-Mahdi Tasha <imwhdiiii@gmail.com>
2026-07-15 12:21:01 +0330

    Fixed typo in footer section

--- component/section/footer.tsx
+++ component/section/footer.tsx
@@ -11,8 +11,7 @@ export default function Footer({ className }: ClassOnlyProps) {
       <footer className={cn("border-t p-4 border-t-foreground/20", className)}>
          <main className="max-w-2xl w-full prose prose-neutral dark:prose-invert mx-auto">
             <h4 className="lg:text-center">
-               copyright: © {moment().format("YYYY")} Mahdi Tasha. All rights
-               reserved.
+               © {moment().format("YYYY")} Mahdi Tasha. All rights reserved.
             </h4>
             <h6 className="mt-2 lg:text-center">
                Built with Next.js, TypeScript & Tailwind CSS — hosted on Vercel.
