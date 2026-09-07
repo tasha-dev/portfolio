5abac8f97a41f7aaf4cdccf63f7eb54145676d5c
Mohamad-Mahdi Tasha <imwhdiiii@gmail.com>
2026-09-07 15:30:07 +0330

    changed order of sections

--- app/page.tsx
+++ app/page.tsx
@@ -17,11 +17,11 @@ export default function HomePage() {
       <article className="lg:space-y-0 space-y-10">
          <Header />
          <Hero />
-         <Skills className="lg:mb-8 mb-5 lg:mt-0 mt-5" />
-         <Projects className="lg:border-0 border-t border-t-foreground/20" />
-         <Experience className="lg:border-0 border-t border-t-foreground/20" />
+         <Skills className="lg:border-0 border-t border-t-foreground/20" />
          <WhatICanBuild className="lg:border-0 border-t border-t-foreground/20" />
+         <Projects className="lg:border-0 border-t border-t-foreground/20" />
          <Proof className="lg:border-y border-t border-foreground/20" />
+         <Experience className="lg:border-0 border-t border-t-foreground/20" />
          <Contact className="lg:border-0 border-t border-t-foreground/20" />
          <Footer />
       </article>
