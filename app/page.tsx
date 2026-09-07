737fe03df5b7c4f2e141047f945a3a22d8efa7f4
Mohamad-Mahdi Tasha <imwhdiiii@gmail.com>
2026-09-07 12:52:59 +0330

    Added what i can build section

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,7 @@ import { Projects } from "@/component/section/projects";
 import Proof from "@/component/section/proof";
 import Skills from "@/component/section/skils";
 import Experience from "@/component/section/experience";
+import WhatICanBuild from "@/component/section/whatICanBuild";
 
 // Creating and exporting Home page as default
 export default function HomePage() {
@@ -19,6 +20,7 @@ export default function HomePage() {
          <Skills className="lg:mb-8 mb-5 lg:mt-0 mt-5" />
          <Projects className="lg:border-0 border-t border-t-foreground/20" />
          <Experience className="lg:border-0 border-t border-t-foreground/20" />
+         <WhatICanBuild className="lg:border-0 border-t border-t-foreground/20" />
          <Proof className="lg:border-y border-t border-foreground/20" />
          <Contact className="lg:border-0 border-t border-t-foreground/20" />
          <Footer />
